4589bd76f10e8d43cdb595039bcd103eb19be24a
guillemlloret <guillem.lloret.macia@estudiantat.upc.edu>
2025-06-06 11:41:19 +0200

    cambios script + Fases y gameHandler

--- Assets/Scripts/Arduino.cs
+++ Assets/Scripts/Arduino.cs
@@ -15,9 +15,14 @@ public class Arduino : MonoBehaviour
 	public int baudRate = 9600;
 	public string str;
 	
-	public static int ValorArduinoSensor;
-	public int sensor;
-    SerialPort arduinoSerial = new SerialPort("COM5", 9600);
+	private int ValorArduinoSensor;
+	private int ValorArduinoSensorAnterior=0;
+	private int ValorDistanciaPlayer1;
+    private int ValorDistanciaPlayer2;
+
+    public int sensor;
+	private int Fase=0; //Fase 0 = juego no comenzado Fase 1 = Placement 1, Fase 2 Placement 2 ...
+    SerialPort arduinoSerial = new SerialPort("COM9", 9600);
 
     void Start()
 	{
@@ -37,10 +42,10 @@ public class Arduino : MonoBehaviour
 
 	void Update()
 	{
-		//ReadFromArduino();
+		ReadFromArduino();
 
-		str = arduinoSerial.ReadLine();
-		Debug.Log(str);
+		//str = arduinoSerial.ReadLine();
+		//Debug.Log(str);
 		
 
 	}
@@ -58,6 +63,8 @@ public class Arduino : MonoBehaviour
 				//sensor = ValorArduinoSensor;
 				if(ValorArduinoSensor !=0)
                 {
+					CheckArduinoNumber(str);
+					GameHandler(str);
 					//Debug.Log(ValorArduinoSensor);
 				}
 				
@@ -67,4 +74,76 @@ public class Arduino : MonoBehaviour
 		{
 		}
 	}
+
+    private void CheckArduinoNumber(string str)
+    {
+		if(str == "999")
+		{
+            Fase = 0;
+            Debug.Log("Comienza placement player 1");
+		}
+        if (str == "010101")
+		{
+			Fase= 1;
+            Debug.Log("Placement player 1");
+            //despues Placement 1 / antes ataque 1
+        }
+        if (str == "666")
+        {
+            Fase = 2;
+            Debug.Log("Comienza placement player 2");
+        }
+        if (str == "020202")
+        {
+			Fase = 3;
+            //despues Placement 2 / antes ataque 1
+        }
+        if (str == "151515")
+        {
+			Fase = 4;
+            //antes ataque 1
+        }
+        if (str == "121212")
+        {
+            Fase = 5;
+            //despues ataque 1 / antes ataque 1
+        }
+        if (str == "252525")
+        {
+            Fase = 6;
+            //antes ataque 2
+        }
+        if (str == "212121")
+        {
+            Fase = 7;
+            //despues ataque 2 / antes ataque 1
+        }
+        if (str == "555")
+        {
+            //despues Win
+        }
+        if (str == "444")
+        {
+            //despues Water
+        }
+    }
+
+	private void GameHandler(string str)
+	{
+		ValorArduinoSensorAnterior = ValorArduinoSensor;
+		Int32.TryParse(str, out ValorArduinoSensor);
+		if(Fase ==1)
+		{
+			Debug.Log(str);
+		}
+		if(Fase == 2)
+		{
+			if(ValorArduinoSensorAnterior != 010101)
+			{
+				ValorDistanciaPlayer1 = ValorArduinoSensorAnterior;
+				Debug.Log(ValorDistanciaPlayer1);
+			}
+		}
+
+    }
 }
